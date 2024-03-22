ee31d7bb78d11724f3821968883099905c847d51
Anna_AIM <KlimovaAnnaA@aimmngt.com>
2024-03-22 12:57:16 +0300

    Work Fri 22/03/24

--- OCP/Defs.py
+++ OCP/Defs.py
@@ -7,6 +7,7 @@
 # from Defs import Period
 # from Defs import new_list
 # from Defs import export_from_RISKCUSTOM
+# from Defs import add_in_currency_column
 
 import pandas as pd
 import numpy as np
@@ -75,5 +76,38 @@ def export_from_RISKCUSTOM(query):
     oracledb.init_oracle_client('C:\\Users\\KlimovaAnnaA\\Documents\\MyFiles\\Oracle\\instantclient_21_13')
     connection = oracledb.connect(user="RISKCUSTOM", password="xxRiscRccess174!", host="exatest2-scan.moscow.eurochem.ru", port=1521, service_name='riskdev.moscow.eurochem.ru', disable_oob= True)
     data_export = pd.read_sql(query, con=connection)
+    connection.close()
     return data_export
+
+# Создание столбца в нужной валюте
+def add_in_currency_column(df, col_with_CCY, col_with_VAL, CCY_to, day_for_export : str = '29/02/24'):
+    data_CCY_map = pd.read_csv('C:\\Users\\KlimovaAnnaA\\Documents\\MyFiles\\Projects\\OCP\\Методология\\CCY_mapping.csv')
+    
+    query = f"""
+    select * from "RISKACCESS"."XXMR_MADAB_CONTENT"
+    where
+    "RISKACCESS"."XXMR_MADAB_CONTENT"."COMMODITY_ID" in (2354,322,311,312,314,315,318,2332,2334,2360,9321,9326,9331,9902,10014,7647,33051,9318,9886,2362,33447) and
+    "RISKACCESS"."XXMR_MADAB_CONTENT"."PERIOD" = TO_DATE('{day_for_export}', 'DD/MM/YY')
+    """
+    # Смотри запрос
+
+    data_export = export_from_RISKCUSTOM(query)[['COMMODITY_ID', 'VALUE1']]
+    values_data = data_export.merge(data_CCY_map, how='left', left_on='COMMODITY_ID', right_on='id', validate='one_to_one')[['VALUE1','CCY_from', 'CCY_to']] 
+    # Может возникнуть ошибка, если значений будет больше
+
+    coef_dict = {}
+    coef_dict[CCY_to] = 1
+    for CCY_from in df[col_with_CCY].unique():
+        if CCY_from != CCY_to:
+            if CCY_from in values_data.CCY_from.tolist():
+                coef_dict[CCY_from] = values_data.query('CCY_from == @CCY_from & CCY_to == @CCY_to').VALUE1.tolist()[0]
+            if CCY_from in values_data.CCY_to.tolist():
+                coef_dict[CCY_from] = 1/values_data.query('CCY_to == @CCY_from & CCY_from == @CCY_to').VALUE1.tolist()[0]
+            else:
+                continue
+
+    df[f'Coef_to_{CCY_to}'] = df[col_with_CCY].replace(coef_dict)
+    df[f'{col_with_VAL}_in_{CCY_to}'] = df[col_with_VAL] * df[f'Coef_to_{CCY_to}']
+
+    return df
 
