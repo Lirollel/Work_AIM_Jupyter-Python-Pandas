b1e8a070e3e17383f2004a4e61f4346d392b2cf1
Anna_AIM <KlimovaAnnaA@aimmngt.com>
2024-04-25 08:22:25 +0300

    24.04.24

--- Liquidity/Liquidity_script.py
+++ Liquidity/Liquidity_script.py
@@ -1,12 +1,11 @@
 print('The "Liquidity" just started running')
 
-
-#Выбор условий выполнения скрипта:
+# Выбор условий выполнения скрипта:
 Print_to_excel = True # Создать excel-файл с расчетами? True/False
 Print_qualuty_check = True # Вынести QC в отдельный excel-файл? True/False
 Send_mail = True # Создать и отправить письмо с расчетами и графиком? True/False
 Send_QC_mail = True # Создать и отправить письмо для QC? True/False
-
+mail_to = 'TarakanovMIu@aimmngt.com' # Получатель письма
 
 # Необходимые импорты:
 import pandas as pd
@@ -14,9 +13,9 @@ import numpy as np
 from datetime import date, timedelta
 import seaborn as sns
 import matplotlib.pyplot as plt
+import matplotlib.axes as ax
 import openpyxl
 from openpyxl.drawing.image import Image
-
 import sys
 sys.path.append("C:\\Users\\KlimovaAnnaA\\Documents\\MyFiles\\Projects\\OCP")
 from Defs import merge_SalesUnits
@@ -26,7 +25,6 @@ from Defs import new_list
 from Defs import export_from_RISKCUSTOM
 from Defs import add_in_currency_column
 from Defs import concat_columns
-
 
 # Cash:
 query = """
@@ -74,32 +72,75 @@ data_total[['cash', 'available_ovedraft', 'total']] = data_total[['cash', 'avail
 data_total['day_of_week'] = data_total.reportDate.dt.day_of_week
 data_total.head(2)
 data_cash.shape, data_overdraft.shape, data_total.shape
+data_total_dates_sql = [f"TO_DATE('{x}','YYYY-MM-DD')" for x in data_total.reportDate.astype(str).unique().tolist()]
+data_total_dates_sql = str(data_total_dates_sql).replace('"','')[1:-1]
 
+query = f"""SELECT * FROM "RISKACCESS"."sapPositionArrears" 
+WHERE "reportDate" in ({data_total_dates_sql})
+and "remainingTermDays" <= 7
+AND "productType" in (130,131,132,133)"""
+# "reportDate" >= trunc(sysdate) and "reportDate" <= trunc(sysdate + interval '7' day)
+data_Debt = export_from_RISKCUSTOM(query)
+
+data_work_Debt = data_Debt[['reportDate', 'companyName', 'businessPartnerName', 'relationshipPartner', 'productTypeName', 'accountAssignmentLinkName', 'termEnd', 'remainingTermDays','transactionTypeName', 'positionCurrency', 'purchaseValuePositionCurrency', 'purchaseValueDisplayCurrency']]
+
+data_work_Debt['amount'] = data_work_Debt.purchaseValuePositionCurrency * np.sign(data_work_Debt.purchaseValueDisplayCurrency)
+
+data_work_Debt['company_id'] = merge_Mapping(col='companyName', df=data_work_Debt)
+data_work_Debt['cty_id'] = merge_Mapping(col='businessPartnerName', df=data_work_Debt)
+data_work_Debt['company_holding'] = merge_SalesUnits(col='company_id', df=data_work_Debt, merge_col='holding')
+data_work_Debt['cty_holding'] = merge_SalesUnits(col='cty_id', df=data_work_Debt, merge_col='holding')
+data_work_Debt['holding_is_equal'] = data_work_Debt['company_holding'] == data_work_Debt['cty_holding']
+data_work_Debt.loc[data_work_Debt.company_holding == 'External', 'holding_is_equal'] = False # Если external == External
+
+data_work_Debt = data_work_Debt[(data_work_Debt.relationshipPartner.isin(['Прочие'])) & (data_work_Debt.holding_is_equal == False)] # Фильтр на внешние сделки
+data_work_Debt = data_work_Debt[np.sign(data_work_Debt.amount) == -1]
+data_work_Debt = add_in_currency_column(data_work_Debt, CCY_to='USD', col_with_CCY= 'positionCurrency', col_with_VAL='amount', DATE='reportDate', date_is_column=True)
+data_work_Debt_group = data_work_Debt.groupby(['reportDate', 'company_holding'], as_index=False).agg({'amount_in_USD':'sum'}).rename(columns={'amount_in_USD':'debt'})
+
+data_total = concat_columns(data_total, ['reportDate', 'holding']).reset_index(drop=True)
+data_work_Debt_group = concat_columns(data_work_Debt_group, ['reportDate', 'company_holding']).reset_index(drop=True)
+
+data_total['debt_outflow'] = data_total.merge(data_work_Debt_group, right_on='concat_columns', left_on='concat_columns', how='left').iloc[:, -1]
+data_total.debt_outflow = data_total.debt_outflow.fillna(0).astype(int).apply(lambda z: z/10**6)
+data_total['debt_abs'] = data_total.debt_outflow.abs()
+data_total['threshold_debt'] = data_total.threshold + data_total.debt_abs
+data_total['early_trigger_debt'] = data_total.early_trigger + data_total.debt_abs
+
+data_total[data_total.debt_outflow != 0].head(2)
 
 # Графики:
 for i in ['EUROCHEM', 'SUEK']:
     data_plot = data_total.query("holding==@i")
-    xticks = data_plot.loc[data_plot.day_of_week.isin([4]), 'reportDate'].sort_values().tolist() # Каждая n-ый день недели в качестве делелния на оси X
-    # xticks = data_plot.reportDate.tolist()[::4] # Каждая n-я дата в качестве делелния на оси X
-    title = i + ' RUS (mln USD)'
-    ax = data_plot.plot(x='reportDate', y=['available_ovedraft', 'cash'], kind='area', stacked=True, figsize=[9, 5], title=title, color=['#8faadc', '#c5e0b4'], xticks=xticks)
-    ax.axhline(y=data_plot.early_trigger.tolist()[0], color='#ffe9a6', label='early_trigger')
-    ax.axhline(y=data_plot.threshold.tolist()[0], color='#f5bb92', label='threshold')
-    handles, _ = ax.get_legend_handles_labels()
-    ax.legend(handles = handles[0:], labels=['overdraft', 'cash','early trigger', 'threshold'])
-    start, end = data_plot.reportDate.min(), data_plot.reportDate.max()
-    ax.set_xlim([start, end])
-    ax.set_axisbelow ( True )
-    ax.grid(axis='y')
-    ax.set(xlabel=None)
-        # Добавление второй оси y
-    ymin, ymax = ax.get_ylim()
-    ax2 = ax.twinx()
-    ax2.set_ylim(ymin, ymax)
+    fig, ax = plt.subplots(figsize=(9, 5)) # размер графика
+    # Построение фигур и линий
+    ax.plot(data_plot.reportDate, data_plot.total, color='#c5e0b4')
+    ax.plot(data_plot.reportDate, data_plot.available_ovedraft, color='#8faadc')
+    ax.fill_between(data_plot.reportDate, data_plot.total, color='#c5e0b4', label='cash')
+    ax.fill_between(data_plot.reportDate, data_plot.available_ovedraft, color='#8faadc', label='overdraft')
+    ax.plot(data_plot.reportDate, data_plot.early_trigger_debt, color='#ffe9a6', label='early trigger')
+    ax.plot(data_plot.reportDate, data_plot.threshold_debt, color='#f5bb92', label='threshold')
+    ax.plot(data_plot.reportDate, data_plot.debt_abs, color='#D1D0D0', label='debt outflow')
+    ax.legend(loc='upper right')
+    # сетка
+    ax.grid(axis='y') 
+    ax.set_axisbelow(True)
+    # растянуть график по x
+    xmin, xmax = data_plot.reportDate.min(), data_plot.reportDate.max() 
+    ax.set_xlim([xmin, xmax])
+    # растянуть график по y
+    ymin, ymax = data_plot.debt_abs.min(), data_plot.total.max() + 50
+    ax.set_ylim([ymin, ymax]) 
+    ax.twinx().set_ylim(ymin, ymax) # добавить вторую ось y
+    # Каждая n-ый день недели в качестве делелния на оси 
+    xticks = data_plot.loc[data_plot.day_of_week.isin([4]), 'reportDate'].sort_values().tolist()
+    plt.xticks(xticks, rotation=45, ha="right")
+    xticks_labels = data_plot.loc[data_plot.day_of_week.isin([4]), 'reportDate'].astype(str).sort_values().tolist()
+    ax.set_xticklabels( xticks_labels, rotation=45, ha="right" )
+    plt.title(f'{i} RUS (mln USD)') # название графика
     
+    plt.tight_layout()
     plt.savefig(f'{i}.png')
-
-
 
 # Запись в файл:
 TD = data_total.reportDate.max()
@@ -109,7 +150,7 @@ Output_file_ECH = TD_str + '_Oper_liquidity_EUROCHEM.xlsx'
 
 if Print_to_excel == True:
   for i in ['EUROCHEM', 'SUEK']:
-      data_print = data_total.query("holding==@i")[['reportDate', 'cash', 'available_ovedraft', 'total', 'early_trigger', 'threshold']]
+      data_print = data_total.query("holding==@i")[['reportDate', 'cash', 'available_ovedraft', 'total', 'debt_outflow','early_trigger', 'threshold']]
       Output_file = TD_str + '_Oper_liquidity_' + i + '.xlsx'
       data_print.to_excel(Output_file, index=False)
       # Create a new or open existing Excel file with openpyxl
@@ -127,9 +168,7 @@ if Print_to_excel == True:
         ws.add_image(img)
       # Save the Excel file (overwrite if existing)
       wb.save(Output_file)
-      wb.close()
 data_total.head(2)
-
 
 # Quality_ckeck.Missing_records
 query = """
@@ -153,13 +192,11 @@ missing_records = [i for i in data_QC_cash_pre_last_date if i not in data_QC_cas
 data_missing_records = data_QC_cash[(data_QC_cash.missing_record.isin(missing_records)) & (data_QC_cash.reportDate == pre_last_date)]
 data_missing_records.head(2)
 
-
 # Quality_ckeck.New_records
 new_records = [i for i in data_QC_cash_last_date if i not in data_QC_cash_pre_last_date]
 
 data_new_records = data_QC_cash[(data_QC_cash.missing_record.isin(new_records)) & (data_QC_cash.reportDate == last_date)]
 data_new_records.head(2)
-
 
 # Quality_ckeck.Mapping
 data_QC_cash['Map_holding'] = merge_SalesUnits(data_QC_cash, col='buCode', merge_col='holding')
@@ -167,7 +204,6 @@ data_QC_cash['Map_holding_2'] = merge_SalesUnits(data_QC_cash, col='buCode', id_
 data_QC_cash.loc[data_QC_cash.Map_holding_2 == 'External' ,'Map_holding_2'] = data_QC_cash.loc[data_QC_cash.Map_holding_2 == 'External' ,'Map_holding']
 data_map_holding = data_QC_cash.loc[(data_QC_cash.Map_holding_2 != data_QC_cash.holding) & (data_QC_cash.reportDate == data_QC_cash.reportDate.max()), ['buCode', 'holding', 'Map_holding_2']].drop_duplicates()
 data_map_holding
-
 
 # Запись QC в файл:
 str(last_date)[:10]
@@ -177,7 +213,6 @@ if Print_qualuty_check == True :
     data_missing_records.to_excel(Output_file, sheet_name='Пропавшие_счета')
     new_list(data_new_records, Output_file, sheet_name='Новые_счета', index=True)
     new_list(data_map_holding, Output_file, sheet_name='Поиск_в_SalesUnits')
-
 
 # Создание письма:
 import win32com.client as win32
@@ -200,7 +235,7 @@ for i in ['EUROCHEM', 'SUEK']:
     Maksim Tarakanov<br><br>
     Whatsapp: +7 915 161 29 12<br>
     Financial risk management</p></body></html>"""
-    mailItem.To = 'TarakanovMIu@aimmngt.com' # mail to
+    mailItem.To = mail_to # mail to
     mail_from = 'KlimovaAnnaA@aimmngt.com' # mail from
         # mail attachment
     if i == 'SUEK':
@@ -232,7 +267,7 @@ Best regards,<br>
 Maksim Tarakanov<br><br>
 Whatsapp: +7 915 161 29 12<br>
 Financial risk management</p></body></html>"""
-mailItem.To = 'TarakanovMIu@aimmngt.com' # mail to
+mailItem.To = mail_to # mail to
 mail_from = 'KlimovaAnnaA@aimmngt.com' # mail from
 # mail attachment
 mail_attachment = Output_file 
@@ -247,6 +282,5 @@ mailItem.Sensitivity  = 2
 mailItem.Display()
 if Send_QC_mail == True:
     mailItem.Send()
-
 
 print('The "Liquidity" was finished')
